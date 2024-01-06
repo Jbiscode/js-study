3f5ceade629c69ff81f9447ca84c389f19598bc2
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-01-07 00:36:27 +0900

    map을 이용해서 동적으로 데이터 할당 : key를 넣어 구분

--- React-CH3-37-starting-project/src/App.jsx
+++ React-CH3-37-starting-project/src/App.jsx
@@ -33,7 +33,8 @@ function App() {
                 <section id="core-concepts">
                     <h2>Core Concepts</h2>
                     <ul>
-                        {/* // map을 이용해서 반복문을 돌릴 수 있음 */}
+                        {/* // map을 이용해서 반복문을 돌릴 수 있음 : key를 추가해줘야 리액트가 내용물이 고유한지 확인할 수 있다고함
+                        요소들을 동적으로 리스트에 출력할 때는, 모든 요소는 고유한 “키”를 받아서 리액트에게 리스트 아이템 아이템을 구분 짓도록 알려준다*/}
                         {CORE_CONCEPTS.map((conceptItem) => (
                             <CoreConcept key={conceptItem.title} {...conceptItem}></CoreConcept>
                         ))}
