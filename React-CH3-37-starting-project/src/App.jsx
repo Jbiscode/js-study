3e0404cd5b353461a6ecc0066743d36214e86788
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-01-06 23:32:31 +0900

    React useState,Hooks기본개념 /

--- React-CH3-37-starting-project/src/App.jsx
+++ React-CH3-37-starting-project/src/App.jsx
@@ -1,19 +1,69 @@
+// CORE_CONCEPTS 내에서 불러와서 필요없어짐
+// import componentsImg from "./assets/components.png";
+import { useState } from "react";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    const [selectedTopic, setSelectedTopic] = useState();
+    // let tabContent = "Please click a tab button to see the content.";
+    function HandleSelect(selectedButton) {
+        setSelectedTopic(selectedButton);
+        // console.log(selectedTopic);
+    }
+
+    let tabContent = <p>Please select a topic</p>;
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
+        );
+    }
+    return (
+        <div>
+            <Header></Header>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}></CoreConcept>
+                        <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
+                        <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
+                        <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={() => HandleSelect("components")}>Components</TabButton>
+                        <TabButton onSelect={() => HandleSelect("jsx")}>JSX</TabButton>
+                        <TabButton onSelect={() => HandleSelect("props")}>Props</TabButton>
+                        <TabButton onSelect={() => HandleSelect("state")}>State</TabButton>
+                    </menu>
+                    {/* 상황에 따라서 다른 컴포넌트를 불러오는 방법 : 삼항연산자 */}
+                    {/* {!selectedTopic ? (
+                        <p>Please select a topic</p>
+                    ) : (
+                        <div id="tab-content">
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
+                        </div>
+                    )} */}
+                    {tabContent}
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
