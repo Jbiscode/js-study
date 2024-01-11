09f06a462368ebb5cafa3d84b0ff1a7b84597d87
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-01-11 18:58:33 +0900

    섹션4-81 (상태끌어올리기) Lifting state up

--- TicTacTo-React/src/App.jsx
+++ TicTacTo-React/src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+
 function App() {
+    const [activePlayer, setActivePlayer] = useState('X');
+
+    function handleSelectSquare() {
+        setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    }
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
-                    <Player initialName="Player1" symbol="X"></Player>
-                    <Player initialName="Player2" symbol="O"></Player>
+                <ol id="players" className="highlight-player">
+                    <Player initialName="Player1" symbol="X" isActive={activePlayer==='X'}></Player>
+                    <Player initialName="Player2" symbol="O" isActive={activePlayer==='O'}></Player>
                 </ol>
-                GAME BOARD
+                <GameBoard onSelectSquare ={handleSelectSquare} activePlayerSymbol={activePlayer} />
             </div>
             LOG
         </main>
