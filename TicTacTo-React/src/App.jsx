b501abdf74aa6d8d7fb12dda94ca47025a268997
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-01-19 21:07:23 +0900

    섹션4-84 (속성에서 상태 파생하기)

--- TicTacTo-React/src/App.jsx
+++ TicTacTo-React/src/App.jsx
@@ -1,21 +1,35 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import LOG from "./components/LOG";
 
 function App() {
-    const [activePlayer, setActivePlayer] = useState('X');
+    const [activePlayer, setActivePlayer] = useState("X");
+    const [gameTurns, setGameTurns] = useState([]);
 
-    function handleSelectSquare() {
+    function handleSelectSquare(rowIndex, colIndex) {
         setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+        setGameTurns((prevTurns) => {
+            let currentPlayer = "X";
+
+            if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+                currentPlayer = "O";
+            }
+            const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, 
+                ...prevTurns, ];
+
+            return updatedTurns;
+        });
     }
+
     return (
         <main>
             <div id="game-container">
                 <ol id="players" className="highlight-player">
-                    <Player initialName="Player1" symbol="X" isActive={activePlayer==='X'}></Player>
-                    <Player initialName="Player2" symbol="O" isActive={activePlayer==='O'}></Player>
+                    <Player initialName="Player1" symbol="X" isActive={activePlayer === "X"}></Player>
+                    <Player initialName="Player2" symbol="O" isActive={activePlayer === "O"}></Player>
                 </ol>
-                <GameBoard onSelectSquare ={handleSelectSquare} activePlayerSymbol={activePlayer} />
+                <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
             </div>
             LOG
         </main>
