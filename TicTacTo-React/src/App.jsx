8452437528a619638b6c4a7820cee05a3468b7a9
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-01-22 18:55:40 +0900

    섹션4-88 (분리된 파일로 데이터 아웃소싱)

--- TicTacTo-React/src/App.jsx
+++ TicTacTo-React/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import LOG from "./components/LOG";
+import {WINNING_COMBINATIONS} from "./winning-combinations";
+
 function deriveActivePlayer(gameTurns) {
     let currentPlayer = "X";
 
