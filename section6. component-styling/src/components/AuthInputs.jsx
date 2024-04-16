7f9511918f48beb27e32c5aa7be365970a828733
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-16 22:01:30 +0900

    섹션6 tailwindCss로 변환

--- section6. component-styling/src/components/AuthInputs.jsx
+++ section6. component-styling/src/components/AuthInputs.jsx
@@ -31,8 +31,9 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" 
+    className="w-full max-w-sm mx-auto p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           invalid={emailNotValid}
           label="email"
@@ -45,9 +46,9 @@ export default function AuthInputs() {
           type="password"
           onChange={(event) => handleInputChange("password", event.target.value)}
         />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
