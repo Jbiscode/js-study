08ea0e7a706b6b7ffdba742e2a6f6163f0752662
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-18 11:52:04 +0900

    섹션9. 새로운 프로젝트 관리 컴포넌트 및 버튼, 인풋 컴포넌트 추가

--- section9. projectManageApp/src/App.jsx
+++ section9. projectManageApp/src/App.jsx
@@ -1,3 +1,5 @@
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
 
 function App() {
@@ -5,6 +7,7 @@ function App() {
     <>
       <main className="h-screen my-8 flex gap-8">
         <ProjectSidebar />
+        <NoProjectSelected />
       </main>
     </>
   );
---
+++ section9. projectManageApp/src/components/NoProjectSelected.jsx
@@ -0,0 +1,20 @@
+import noProjectImage from "../assets/no-projects.png";
+import Button from "./Button";
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjectImage}
+        alt=""
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        선택된 프로젝트가 없습니다.
+      </h2>
+      <p className="text-stone-400 mb-4">프로젝트를 선택하거나, 생성해주세요</p>
+      <p className="mt-8">
+        <Button >프로젝트 생성하기</Button>
+      </p>
+    </div>
+  );
+}
