f2a2469449a3928404ec9706f4947a4e730df083
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-19 15:26:27 +0900

    섹션9. 입력없으면 모달창으로 경고창띄우기 컴포넌트

--- section9. projectManageApp/src/components/NewProject.jsx
+++ section9. projectManageApp/src/components/NewProject.jsx
@@ -1,7 +1,11 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
+
 
 export default function NewProject({onAddClick}) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const duedate = useRef();
@@ -11,6 +15,15 @@ export default function NewProject({onAddClick}) {
     const enteredDescription = description.current.value;
     const enteredDueDate = duedate.current.value;
 
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredDueDate.trim().length === 0
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAddClick({
       title: enteredTitle,
       description: enteredDescription,
@@ -19,24 +32,30 @@ export default function NewProject({onAddClick}) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">취소</button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}>
-            등록
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={duedate} label="Due Date" />
+    <>
+    <Modal ref={modal} buttonCaption="닫기">
+      <h2>입력값을 확인하세요</h2>
+      <p>뭔가를 빠트린거같아요!</p>
+    </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">취소</button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}>
+              등록
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={duedate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
