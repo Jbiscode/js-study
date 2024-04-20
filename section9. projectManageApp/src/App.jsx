d8c3bb7dfac11ee09be256a910cf01e21c89c00a
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-20 22:00:18 +0900

    섹션9. 새로운 작업 추가 컴포넌트와 작업 목록 컴포넌트 추가

--- section9. projectManageApp/src/App.jsx
+++ section9. projectManageApp/src/App.jsx
@@ -3,33 +3,43 @@ import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
-    currentAction: "nothing-selected",
     selectedProjectId: undefined,
     projects: [],
+    tasks: [],
   });
 
-  let content;
-
+  // 프로젝트 추가시작 버튼을 클릭했을 때 실행되는 함수
   function handleStartAddProject() {
     setProjectsState((prevState) => {
       return {
         ...prevState,
-        currentAction: "add-project",
+        selectedProjectId: null,
       };
     });
   }
+  // 프로젝트 추가 취소 버튼을 클릭했을 때 실행되는 함수
   function handleCancelAddProject() {
     setProjectsState((prevState) => {
       return {
         ...prevState,
-        currentAction: "nothing-selected",
+        selectedProjectId: undefined,
       };
     });
   }
-
+  // 프로젝트 선택시 실행되는 함수
+  function handleSelectProject(projectId) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: projectId,
+      };
+    });
+  }
+  // 프로젝트 추가시 실행되는 함수
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
       const newProject = {
@@ -40,23 +50,83 @@ function App() {
       return {
         ...prevState,
         projects: [...prevState.projects, newProject],
-        currentAction: "nothing-selected",
+        selectedProjectId: undefined,
       };
     });
   }
-  console.log(projectsState);
-
-  if (projectsState.currentAction === "nothing-selected") {
-    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
-  } else if (projectsState.currentAction === "add-project") {
-    content = <NewProject onAddClick={handleAddProject} onCancelClick={handleCancelAddProject} />;
+  /**
+   * 프로젝트 삭제시 실행되는 함수
+   */
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+        selectedProjectId: undefined,
+      };
+    });
   }
+  // 태스크 추가시 실행되는 함수
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = (Math.random() * 100000000).toFixed(0);
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+  // 태스크 삭제시 실행되는 함수
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+  let content;
+
+  if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else if (projectsState.selectedProjectId === null) {
+    content = (
+      <NewProject
+        onAddClick={handleAddProject}
+        onCancelClick={handleCancelAddProject}
+      />
+    );
+  } else {
+    content = (
+      <SelectedProject
+        project={selectedProject}
+        onDeleteClick={handleDeleteProject}
+        onTaskAdd={handleAddTask}
+        onTaskDelete={handleDeleteTask}
+        tasks={projectsState.tasks}
+      />
+    );
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
         <ProjectSidebar
           onStartAddProject={handleStartAddProject}
           projects={projectsState.projects}
+          onSelectProject={handleSelectProject}
+          selectedProjectId={projectsState.selectedProjectId}
         />
         {content}
       </main>
