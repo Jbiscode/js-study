cd435951b2c47b08cb4ea4d6a71f409d75dc1cb6
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-25 11:39:18 +0900

    섹션11. useEffect의존성 이해하기&ESC로 모달닫을때 버그 수정

--- section11. SideEffectsuseEffect/src/App.jsx
+++ section11. SideEffectsuseEffect/src/App.jsx
@@ -12,7 +12,7 @@ const storedPlaces = storedPlaceIds.map((id) =>
   AVAILABLE_PLACES.find((place) => place.id === id)
 );
 function App() {
-  const modal = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
@@ -30,12 +30,14 @@ function App() {
   }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    // modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    // modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -59,7 +61,8 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    // modal.current.close();
+    setModalIsOpen(false);
     const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
     localStorage.setItem(
       "selectedPlaces",
@@ -69,7 +72,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal ref={modal} open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
--- section11. SideEffectsuseEffect/src/components/Modal.jsx
+++ section11. SideEffectsuseEffect/src/components/Modal.jsx
@@ -1,26 +1,23 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+// onClose 함수를 받아서 dialog의 onClose 이벤트에 연결해준다. ESC로 닫았을때도 modal을 false로 바꿔주기 위함이다.
+function Modal({ open, onClose, children }) {
   const dialog = useRef();
-
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
-
+  // 여기서는 무한 루프를 방지하기 위한것이 아니라, 특정값을 동기화시키고자 하기때문에 useEffect를 사용한다.
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal")
   );
-});
+}
 
 export default Modal;
