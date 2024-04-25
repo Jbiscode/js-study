a520fd53236a4fca58549eeb04369d10bb801a86
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-25 16:24:17 +0900

    섹션12. 퀴즈 끝났을때 결과창 로딩하기

--- section12. ReactQuiz/src/components/Quiz.jsx
+++ section12. ReactQuiz/src/components/Quiz.jsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(answer) {
     setUserAnswers((prev) => {
       return [...prev, answer];
     });
-    console.log(userAnswers);
   }
+
+  if (quizIsComplete) {
+    return (
+      <>
+        <div id="summary">
+          <img src={quizCompleteImg} alt="" />
+          <h2>Quiz Completed!</h2>
+        </div>
+      </>
+    );
+  }
+  const shuffledAnswers = QUESTIONS[activeQuestionIndex].answers;
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
         <p>{QUESTIONS[activeQuestionIndex].text}</p>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button
                 onClick={() => {
