593410e0bb71c3689832b7d0e7780d3e797b7baa
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-25 15:36:52 +0900

    섹션12. 퀴즈내용가져와서 띄우기

---
+++ section12. ReactQuiz/src/components/Quiz.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(answer) {
+    setUserAnswers((prev) => {
+      return [...prev, answer];
+    });
+    console.log(userAnswers);
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <p>{QUESTIONS[activeQuestionIndex].text}</p>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button
+                onClick={() => {
+                  handleSelectAnswer(answer);
+                }}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
