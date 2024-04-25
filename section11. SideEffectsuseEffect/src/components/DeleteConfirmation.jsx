82cba95b1c2b06d742daea3a60cadda0eb1ca7be
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-25 14:26:15 +0900

    섹션11. ProgressBar분리해서 렌더링 최적화하기

--- section11. SideEffectsuseEffect/src/components/DeleteConfirmation.jsx
+++ section11. SideEffectsuseEffect/src/components/DeleteConfirmation.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
+import ProgressBar from "./ProgerssBar";
+
+const TIMEOUT = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
   useEffect(() => {
     console.log("타이머시작");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMEOUT);
     return () => {
       console.log("타이머종료");
       clearTimeout(timer);
@@ -24,6 +27,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMEOUT} />
     </div>
   );
 }
