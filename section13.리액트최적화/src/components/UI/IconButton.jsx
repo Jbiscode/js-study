678445c4237602b776c88215020752700d2512c5
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-28 18:13:48 +0900

    섹션13. memo hook을사용해서 불필요한 렌더링 제거, IconButton을 누를때마다 재렌더링을 막기위해서 Counter.jsx에 있는 handle함수들을 useCallback으로 고정하고 IconButton을 memo훅으로 감싸서 렌더링 방지
    기본 Counter시작값을 정할때 모든 컴포넌트가 재렌더링되는것을 수정

--- section13.리액트최적화/src/components/UI/IconButton.jsx
+++ section13.리액트최적화/src/components/UI/IconButton.jsx
@@ -1,7 +1,8 @@
-import { log } from '../../log.js';
+import { memo } from "react";
+import { log } from "../../log.js";
 
-export default function IconButton({ children, icon, ...props }) {
-  log('<IconButton /> rendered', 2);
+const IconButton = memo(function IconButton({ children, icon, ...props }) {
+  log("<IconButton /> rendered", 2);
 
   const Icon = icon;
   return (
@@ -10,4 +11,9 @@ export default function IconButton({ children, icon, ...props }) {
       <span className="button-text">{children}</span>
     </button>
   );
-}
+});
+
+export default IconButton;
+// memo 함수를 사용하여 IconButton 컴포넌트를 메모이제이션.
+// 이는 컴포넌트의 렌더링 성능을 향상시키는 데 도움.
+// props가 변경되지 않았다면, React는 이전에 렌더링된 결과를 재사용하여 불필요한 렌더링을 방지
