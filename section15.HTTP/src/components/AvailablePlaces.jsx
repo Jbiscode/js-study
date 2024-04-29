6d99bb1b54328ea96d6e3fa0f2c2da1b638def5a
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-29 16:52:00 +0900

    섹션15. fetch async처리 & 로딩메시지

--- section15.HTTP/src/components/AvailablePlaces.jsx
+++ section15.HTTP/src/components/AvailablePlaces.jsx
@@ -2,23 +2,28 @@ import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setAvailablePlaces(data.places);
-      });
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
-      fallbackText="No places available."
+      isLoading={isFetching}
+      loadingText="장소 불러오는 중..."
+      fallbackText="선택 가능한 장소가 없습니다."
       onSelectPlace={onSelectPlace}
     />
   );
