cb1379ade6321fa1b8019159c59ddafda40424bf
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-29 20:29:24 +0900

    섹션15. fetch api 파일 분리해서 데이터 받아오기

--- section15.HTTP/src/components/AvailablePlaces.jsx
+++ section15.HTTP/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -12,16 +13,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
+        const places = await fetchAvailablePlaces();
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch places.");
-        }
         navigator.geolocation.getCurrentPosition((position) => {
           const { latitude, longitude } = position.coords;
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             latitude,
             longitude
           );
