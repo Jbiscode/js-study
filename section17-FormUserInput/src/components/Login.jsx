14256d5d02034e4238861d9b76d1b7c38940930e
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-04-30 21:39:12 +0900

    섹션17. 이메일&비밀번호 입력받기 기본

--- section17-FormUserInput/src/components/Login.jsx
+++ section17-FormUserInput/src/components/Login.jsx
@@ -1,17 +1,76 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault(); // 기본 제출 동작 방지
+    const form = event.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    console.log("Email:", enteredValues.email);
+    console.log("Password", enteredValues.password);
+  }
+
+  function handleInputChange(event) {
+    const target = event.target;
+    setEnteredValues((prevState) => {
+      return {
+        ...prevState,
+        [target.name]: target.value,
+      };
+    });
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredValues((prevState) => {
+  //     return {
+  //       ...prevState,
+  //       email: event.target.value,
+  //     };
+  //   });
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setEnteredValues((prevState) => {
+  //     return {
+  //       ...prevState,
+  //       password: event.target.value,
+  //     };
+  //   });
+  // }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handleInputChange}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
